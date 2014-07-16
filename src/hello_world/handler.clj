15794f91d3e96ae831f7ed1702bd56e583a70a0c
David Farquharson <djfarquharson@spscommerce.com>
2014-07-16 12:50:02 -0500

    not really the same as /xd/list, but on the right path

--- src/hello_world/handler.clj
+++ src/hello_world/handler.clj
@@ -11,10 +11,13 @@
 (defn repo-exists [repo]
   (.exists (clojure.java.io/as-file (str repo-dir repo))))
 
+(def repo-list (file-seq (clojure.java.io/file repo-dir)))
+
 (defroutes app-routes
   (GET "/" [] "Home Page\n")
   (GET "/hello" [] "Hello world!\n")
   (GET "/hello/:user" [user] (hello user))
+  (GET "/repo" [] (map #(str % "\n") (take 10 repo-list)))
   (GET "/repo-exists/:repo" [repo] (str (repo-exists repo) "\n"))
   (route/resources "/")
   (route/not-found "Not Found"))
